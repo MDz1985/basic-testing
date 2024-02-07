ab0b67c5967509709b8d35fb94e0224cbe9a0c3c
MDz1985 <m_d@mail.uk>
2024-02-07 15:36:58 +0100

    feat: implement first part of the mocking-lib-api tests

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -1,10 +1,16 @@
 // Uncomment the code below and write your tests
-/* import axios from 'axios';
-import { throttledGetDataFromApi } from './index'; */
+import { throttledGetDataFromApi } from './index';
+import axios from 'axios';
+
+// jest.mock('axios');
 
 describe('throttledGetDataFromApi', () => {
   test('should create instance with provided base url', async () => {
-    // Write your test here
+    const baseURL = 'https://jsonplaceholder.typicode.com';
+    const relativePath = '/posts/1';
+    const create = jest.spyOn(axios, 'create');
+    await throttledGetDataFromApi(relativePath);
+    expect(create).toHaveBeenCalledWith({ baseURL });
   });
 
   test('should perform request to correct provided url', async () => {

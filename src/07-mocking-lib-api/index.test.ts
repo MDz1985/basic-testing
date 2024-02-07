04eb124fcf96aa99b3f9bb0a26cf406534a91503
MDz1985 <m_d@mail.uk>
2024-02-07 19:10:57 +0100

    feat: implement second and third part of the mocking-lib-api tests to work separately

--- src/07-mocking-lib-api/index.test.ts
+++ src/07-mocking-lib-api/index.test.ts
@@ -2,8 +2,6 @@
 import { throttledGetDataFromApi } from './index';
 import axios from 'axios';
 
-// jest.mock('axios');
-
 describe('throttledGetDataFromApi', () => {
   test('should create instance with provided base url', async () => {
     const baseURL = 'https://jsonplaceholder.typicode.com';
@@ -14,10 +12,36 @@ describe('throttledGetDataFromApi', () => {
   });
 
   test('should perform request to correct provided url', async () => {
-    // Write your test here
+    const relativePath = '/posts/1';
+    const baseURL = 'https://jsonplaceholder.typicode.com';
+    const mockAxios: typeof axios = jest.genMockFromModule('axios');
+    mockAxios.create = jest.fn(() => mockAxios);
+    const instance = mockAxios.create({
+      baseURL,
+    });
+    const create = jest.spyOn(axios, 'create').mockReturnValue(instance);
+    const get = jest.spyOn(instance, 'get').mockResolvedValue({ data: 'fd' });
+    await throttledGetDataFromApi(relativePath);
+    expect(create).toHaveBeenCalledWith({ baseURL });
+    expect(get).toHaveBeenCalledWith(relativePath);
   });
 
   test('should return response data', async () => {
-    // Write your test here
+    const relativePath = '/posts/1';
+    const baseURL = 'https://jsonplaceholder.typicode.com';
+    const responseData = { testKey: 'testValue' };
+    const mockAxios: typeof axios = jest.genMockFromModule('axios');
+    mockAxios.create = jest.fn(() => mockAxios);
+    const instance = mockAxios.create({
+      baseURL,
+    });
+    const create = jest.spyOn(axios, 'create').mockReturnValue(instance);
+    const get = jest
+      .spyOn(instance, 'get')
+      .mockResolvedValue({ data: responseData });
+    const result = await throttledGetDataFromApi(relativePath);
+    expect(create).toHaveBeenCalledWith({ baseURL });
+    expect(get).toHaveBeenCalledWith(relativePath);
+    expect(result).toEqual(responseData);
   });
 });
